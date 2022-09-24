4c442345e34eebf8c1acdf08d45e16fd10678ec7
Roma <Jambulil@mail.ru>
2022-09-24 13:36:27 +0300

    Использовано foreach;снято ограничеие на длинну  слогаемых

--- calculator.php
+++ calculator.php
@@ -3,13 +3,12 @@
     function calculator(string $countingString): ?string
     {
         if (!preg_match("/[0-9, \- + ]+$/", $countingString)) {
-            echo "Вводите только разрешенные символы";
-            return null;
+            return "Вводите только разрешенные символы";
         }
         $terms = explode("+", $countingString);
         $summ = 0;
         $temp = "";
-
+        $numberOfOperations = 0;
 
         foreach ($terms as $key => &$value) {
             $minusPos = strpos($value, "-");
@@ -18,16 +17,22 @@
                 foreach ($termsMinus as $keyMinus => &$valueMinus) {
                     if ($keyMinus === 0) {
                         $temp = $valueMinus;
+                        $numberOfOperations++;
                     } else {
                         $temp = $temp - $valueMinus;
+                        $numberOfOperations++;
                     }
                 }
                 $summ = $summ + $temp;
             } else {
                 $summ = $summ + $value;
+                $numberOfOperations++;
             }
         }
-
+        if ($numberOfOperations > 5)
+        {
+            return "Введено больше 5 слогаемых";
+        }
         return $summ;
     }
     $enteredString = readline("Введите строку: ");
