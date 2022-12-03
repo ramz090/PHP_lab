98a14575d9dc938c955cef220e780fb91526e143
Roma <Jambulil@mail.ru>
2022-12-03 14:28:49 +0300

    feat(lw2):ignore vendor

--- calculator.php
+++ calculator.php
@@ -1,37 +1,37 @@
 <?php
 $enteredSting = $argv[1];
-    function calculator(string $countingString): string
-    {
-        if (!preg_match("/[0-9, \-+]+$/", $countingString)) {
-            return "Вводите только разрешенные символы";
-        }
-        $terms = explode("+", $countingString);
-        $summ = 0;
-        $temp = "";
-        $numberOfOperations = 0;
-
-        foreach ($terms as $term) {
-            $minusPos = strpos($term, "-");
-            if ($minusPos) {
-                $termsMinus = explode("-", $term);
-                foreach ($termsMinus as $keyMinus => $termMinus) {
-                    if ($keyMinus === 0) {
-                        $temp = $termMinus;
-                        $numberOfOperations++;
-                    } else {
-                        $temp = $temp - (int)$termMinus;
-                        $numberOfOperations++;
-                    }
-                }
-                $summ = $summ + $temp;
-            } else {
-                $summ = $summ + $term;
-                $numberOfOperations++;
-            }
-        }
-        if ($numberOfOperations > 5) {
-            return "Введено больше 5 слогаемых";
-        }
-        return $summ;
+function calculator(string $countingString): string
+{
+    if (!preg_match("/^[0-9a-z_]+$/i", $countingString) || strlen($countingString) > 19) {
+        return "Вводите только разрешенные символы";
     }
-    echo calculator($enteredSting);
+    $terms = explode("+", $countingString);
+    $summ = 0;
+    $temp = "";
+    $numberOfOperations = 0;
+
+    foreach ($terms as $term) {
+        $minusPos = strpos($term, "-");
+        if ($minusPos) {
+            $termsMinus = explode("-", $term);
+            foreach ($termsMinus as $keyMinus => $termMinus) {
+                if ($keyMinus === 0) {
+                    $temp = $termMinus;
+                    $numberOfOperations++;
+                } else {
+                    $temp = $temp - (int)$termMinus;
+                    $numberOfOperations++;
+                }
+            }
+            $summ = $summ + $temp;
+        } else {
+            $summ = $summ + $term;
+            $numberOfOperations++;
+        }
+    }
+    if ($numberOfOperations > 5) {
+        return "Введено больше 5 слогаемых";
+    }
+    return $summ;
+}
+echo calculator($enteredSting);
